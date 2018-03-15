33b02a6edfa6331e1433aae54e3e31debf31f255
Latitude/Timelock <tomekgsd@vp.pl>
2018-03-15 12:54:53 +0100

    akchually no

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,1 +1,23 @@
-s
+import random
+
+try:
+    x=(int)(input("Podaj pierwsza liczbe z zakresu"))
+except ValueError:
+    print("Podano zły format liczby z zakresu 1")
+
+
+try:
+    y=(int)(input("Podaj druga liczbe z zakresu"))
+except ValueError:
+    print("Podano zły format liczby z zakresu 2")
+
+swieradint=random.randint(x,y)
+
+while True:
+    try:
+        z=(int)(input("Zgaduj dalyj"))
+        if (z == swieradint):
+            print("no pienknie")
+            break
+    except ValueError:
+        print("no ale zgaduj zgodnie z zasadami")
