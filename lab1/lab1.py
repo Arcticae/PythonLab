6854c2e841936ae816b34b57198d617c00fa0fea
Latitude/Timelock <tomekgsd@vp.pl>
2018-03-15 15:25:30 +0100

    Added working solutions

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,23 +1,29 @@
 import random
 
 try:
-    x=(int)(input("Podaj pierwsza liczbe z zakresu"))
+    x=int(input("Podaj pierwsza liczbe z zakresu"))
 except ValueError:
     print("Podano zły format liczby z zakresu 1")
-
+    exit()
 
 try:
-    y=(int)(input("Podaj druga liczbe z zakresu"))
+    y=int(input("Podaj druga liczbe z zakresu"))
 except ValueError:
     print("Podano zły format liczby z zakresu 2")
+    exit()
 
-swieradint=random.randint(x,y)
 
-while True:
-    try:
-        z=(int)(input("Zgaduj dalyj"))
-        if (z == swieradint):
-            print("no pienknie")
-            break
-    except ValueError:
-        print("no ale zgaduj zgodnie z zasadami")
+if (x>y):
+    print("Zle podany zakres")
+    exit()
+else:
+    swieradint = random.randint(x,y)
+    while True:
+        try:
+            z=int(input("Zgaduj"))
+            if(z == swieradint):
+                print("no pienknie")
+                exit()
+        except ValueError:
+            print("no ale zgaduj zgodnie z zasadami obrazam sie")
+            exit()
