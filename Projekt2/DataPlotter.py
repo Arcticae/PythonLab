0038b2532ee58f21a53303b1b28f7a71c00103fa
Arcticae <tomekgsd@gmail.com>
2018-06-07 13:48:01 +0200

    All working + tests added

--- Projekt2/DataPlotter.py
+++ Projekt2/DataPlotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv, sys
+import unittest
 
 
 class CSVhandler:  # usage -> this gets data from given csv path in constructor
@@ -23,9 +24,13 @@ class CSVhandler:  # usage -> this gets data from given csv path in constructor
     def __add__(self, other):
         self.data += other.data
 
+    def __str__(self):
+        return str(self.data)
 
-class DataPlotter:
 
+class DataPlotter:  # features -> alcohol dict indicates indices for each alcohol string
+    #           -> colors table are for different colors of plot
+    #             -> data is raw csv data  (with one collumn of headers) for all countries
     def __init__(self, csv_handler):
         self.data = csv_handler.get_data()
         self.colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink',
@@ -33,9 +38,14 @@ class DataPlotter:
         self.alcohol = {'Beer': 1, 'Spirit Drinks': 2, 'Wine': 3, 'Pure Alcohol (litres)': 4}
 
     def show_beverage(self, bev_type="none", *args):
-        if (bev_type == "none") | (len(args) < 1):
-            print("You did not give sufficient parameters to printing method")
-            return
+
+        if (bev_type == "none") | (len(args) < 1) | (len(args) > 10):
+            print("You did not give sufficient/correct parameters to printing method,check your choice once again")
+            return 1
+
+        for country in args:
+            if country not in list(map(lambda x: x[0], self.data)):
+                print("There is no data about " + country + ".")
 
         plotted = list(filter(lambda x: x[0] in args, self.data))
         plotted.sort(key=lambda x: float(x[self.alcohol.get(bev_type)]))
@@ -60,13 +70,18 @@ class DataPlotter:
         for index, value in enumerate(values):
             ax.text(index + 1, value * 1.05, str(value), ha='center')
 
-        plt.xticks(arguments, labels, rotation=90)
+        plt.xticks(arguments, labels, rotation=60)
         plt.tight_layout()
         plt.title(bev_type + " Servings per person in each country")
         plt.show()
-        return
+        return 0
 
     def top_x_countries(self, x, bev_type='none'):
+
+        if (bev_type == 'none') | (bev_type not in self.alcohol.keys()) | (x < 1):
+            print("Not correct beverage type selected: " + bev_type + "\t(Maybe check your spelling?)")
+            return 1
+
         wanted = self.data[1:]
         wanted.sort(key=lambda x: float(x[self.alcohol.get(bev_type)]), reverse=True)
         names = []
@@ -75,15 +90,59 @@ class DataPlotter:
             index += 1
 
         self.show_beverage(bev_type, *names)
-        return
+        return 0
+
+    def bot_x_countries(self, x, bev_type='none'):
+
+        if ( bev_type == 'none' ) | (bev_type not in self.alcohol.keys()) | (x < 1):
+            print("Not correct beverage type selected: " + bev_type + "\t(Maybe check your spelling?)")
+            return 1
+
+        wanted = self.data[1:]
+        wanted.sort(key=lambda x: float(x[self.alcohol.get(bev_type)]))
+        names = []
+        for index in range(0, x):
+            names.append(wanted[index][0])
+            index += 1
+
+        self.show_beverage(bev_type, *names)
+        return 0
 
 
-data_plot = DataPlotter(CSVhandler(sys.argv[1]))
+if len(sys.argv) < 2:
+    print("Give at least one csv file to parse")
+    exit(1)
+
+csvka = CSVhandler(sys.argv[1])
+data_plot = DataPlotter(csvka)
+
 
 data_plot.show_beverage("Spirit Drinks", "Albania", "Poland", "Norway", "USA", "Zimbabwe", "Etiopia")
 data_plot.show_beverage("Beer", "Albania", "Poland", "Norway", "USA", "Zimbabwe", "Etiopia")
-data_plot.show_beverage("Wine", "Albania", "Poland", "Norway", "USA", "Zimbabwe", "Etiopia")
+data_plot.show_beverage("Wine", "Albania", "Poland", "Norway", "USA", "Zimbabwe", "Etiopia", "Nibylandia")
 data_plot.show_beverage("Pure Alcohol (litres)", "Albania", "Poland", "Norway", "USA", "Zimbabwe", "Etiopia")
 
 
-data_plot.top_x_countries(10, "Wine")
+data_plot.top_x_countries(10, "Pure Alcohol (litres)")
+data_plot.bot_x_countries(10, "Beer")
+
+
+
+class MyTests(unittest.TestCase):
+
+    def test_names(self):
+        self.assertEqual(data_plot.show_beverage("Pure Alcohol (litres)", "Albania", "Poland", "Etiopia"), 0)
+        self.assertEqual(data_plot.show_beverage("Pure Alcohol (litry)"), 1)
+
+    def test_quantity(self):
+        self.assertEqual(data_plot.show_beverage("Wine"), 1)
+        self.assertEqual(data_plot.show_beverage("Wine", "Poland"), 0)
+
+    def test_top_number(self):
+        self.assertEqual(data_plot.top_x_countries(-1, "Wine"), 1)
+        self.assertEqual(data_plot.top_x_countries(0, "Wine"), 1)
+        self.assertEqual(data_plot.top_x_countries(1, "Wine"), 0)
+
+
+if __name__ == "__main__":
+    unittest.main(argv=['ignore-first-arg'])
